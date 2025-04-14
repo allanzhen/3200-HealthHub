88b11ca14ec1b9980f88479b38c1af315a94aed0
sn-tn <158114615+sn-tn@users.noreply.github.com>
2025-04-14 16:52:22 -0400

    Completed functionality for user and food list pages

---
+++ app/src/pages/11_User_Data.py
@@ -0,0 +1,69 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import pandas as pd
+import streamlit as st
+from streamlit_extras.app_logo import add_logo
+import matplotlib.pyplot as plt
+import seaborn as sns
+import requests
+from modules.nav import SideBarLinks
+
+# Setup sidebar and page
+SideBarLinks()
+st.header('User Data')
+
+users = requests.get('http://api:4000/a/users').json()
+
+try:
+    st.dataframe(users)
+except:
+    st.write('Could not get user list!')
+
+with st.form("Update a user's email"):
+    email = st.text_input("Input email to be changed:")
+    new_email = st.text_input("Input new email:")
+
+    submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        data = {}
+        data['Email'] = email
+        data['New Email'] = new_email
+        data['Name'] = None
+        data['New Name'] = None
+
+        st.write("Please refresh page to see updated table.")
+
+        requests.put('http://api:4000/a/users', json=data)
+
+with st.form("Update a user's name"):
+    name = st.text_input("Input name to be changed:")
+    new_name = st.text_input("Input new name:")
+
+    submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        data = {}
+        data['Email'] = None
+        data['New Email'] = None
+        data['Name'] = name
+        data['New Name'] = new_name
+
+        st.write("Please refresh page to see updated table.")
+
+        requests.put('http://api:4000/a/users', json=data)
+
+with st.form("Delete a user:"):
+    st.write("***WARNING CANNOT BE UNDONE")
+    email = st.text_input("Email of user to delete")
+
+    submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        data = {}
+        data['Email'] = email
+
+        st.write("Please refresh page to see updated table.")
+
+        requests.delete('http://api:4000/a/users', json=data)
---
+++ app/src/pages/12_Food_List.py
@@ -0,0 +1,39 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import pandas as pd
+import streamlit as st
+from streamlit_extras.app_logo import add_logo
+import matplotlib.pyplot as plt
+import seaborn as sns
+import requests
+from modules.nav import SideBarLinks
+
+# Setup sidebar and page
+SideBarLinks()
+st.header('Food List')
+
+food = requests.get('http://api:4000/a/food_list').json()
+
+try:
+    st.dataframe(food)
+except:
+    st.write('Could not get user list!')
+
+with st.form("Add a food"):
+    id = st.text_input("Input Food ID:")
+    name = st.text_input("Input Food Name:")
+    cals = st.text_input("Input Food Calories:")
+
+    submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        data = {}
+        data['FoodID'] = id
+        data['FoodName'] = name
+        data['Calories'] = cals
+        st.write(data)
+
+        st.write("Please refresh page to see updated table.")
+
+        requests.post('http://api:4000/a/food_list', json=data)
