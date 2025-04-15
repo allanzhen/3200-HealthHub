886b3c8f5058991f51673d49330b8fa81dc7778d
sn-tn <158114615+sn-tn@users.noreply.github.com>
2025-04-15 11:58:35 -0400

    Updated Employee Tickets page to allow user to assign ticket to employee

--- app/src/pages/14_Employee_Tickets.py
+++ app/src/pages/14_Employee_Tickets.py
@@ -19,3 +19,18 @@ try:
     st.dataframe(employee_tickets)
 except:
     st.write('Could not get user list!')
+
+with st.form('Update a ticket\'s status'):
+    id = st.text_input("Input Ticket ID to be assigned:")
+    employee = st.text_input("Input the ID of the employee who will work on this ticket:")
+
+    submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        data = {}
+        data['TicketID'] =  id
+        data['EmployeeID'] = employee
+
+        st.write("Please refresh page to see updated table.")
+
+        requests.post('http://api:4000/a/employee_tix', json=data)
